78df5ecdbeb228e3803223e5a42237998b6cef99
mwojcickaa <martynawojcickaaa@gmail.com>
2022-11-25 21:16:56 +0100

    Fix previous commit, for loop

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,13 @@
 // ZADANIE 1
 let box1 = document.getElementById("zad1")
-let i = 0;
-for (i = 0; i < 10; i++) {
-    for (i = 0; i < 5; i++) {
-    box1.innerHTML += "_ "
+for (let i = 0; i < 10; i++) {
+    if (i < 5) {
+        box1.innerHTML += "_ " 
+    } else {
+        box1.innerHTML += "* "
     }
- for (i = 5; i < 10; i++) {
-    box1.innerHTML += "* "
- }
 }
+
 
 
 
