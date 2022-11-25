879b9a40ff3aebbab2ce94e8e482c64d4578db66
mwojcickaa <martynawojcickaaa@gmail.com>
2022-11-25 18:55:41 +0100

    Make a for loop made of five _ and *

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// ZADANIE 1
+let box1 = document.getElementById("zad1")
+let i = 0;
+for (i = 0; i < 10; i++) {
+    for (i = 0; i < 5; i++) {
+    box1.innerHTML += "_ "
+    }
+ for (i = 5; i < 10; i++) {
+    box1.innerHTML += "* "
+ }
+}
+
+
+
+
