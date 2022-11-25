3926a70fff611ea462a9e8c825206e6d2e6905ff
mwojcickaa <martynawojcickaaa@gmail.com>
2022-11-25 23:44:20 +0100

    Make a for loop that repeats 4 times with a break in between

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,20 @@ for (let i = 0; i < 10; i++) {
         box1.innerHTML += "* "
     }
 }
-
+//ZADANIE 2 
+let string = "";
+for (let j = 0; j < 4; j++) {
+    for (let i = 0; i < 10; i++) {
+        if (i < 5) 
+        {
+        string += "_ "
+        } else {
+        string += "* " 
+        }      
+}
+string = string + "\n"
+}
+document.getElementById("zad2").innerHTML = string;
 
 
 
