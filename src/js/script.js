2db565cc579fe42f1a0866c8904240f1bf4a09bf
mwojcickaa <martynawojcickaaa@gmail.com>
2022-11-28 15:08:26 +0100

    Make half a christmas tree with loops

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,8 @@ for (let i = 0; i < 10; i++) {
         box1.innerHTML += "* "
     }
 }
-//ZADANIE 2 
+
+// ZADANIE 2 
 let string = "";
 for (let j = 0; j < 4; j++) {
     for (let i = 0; i < 10; i++) {
@@ -22,6 +23,17 @@ string = string + "\n"
 }
 document.getElementById("zad2").innerHTML = string;
 
-
-
+// ZADANIE 3
+let string1 = "";
+for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < 10; i++) {
+    if (i < j + 1) {
+        string1 += "*" 
+    } else {
+        string1 += "-"
+    } 
+}
+string1 = string1 + "\n"
+}
+document.getElementById("zad3").innerHTML = string1;
 
