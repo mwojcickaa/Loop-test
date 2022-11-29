34d10f595fc2d439b512b8e0463b6b40626b405c
mwojcickaa <martynawojcickaaa@gmail.com>
2022-11-29 23:10:30 +0100

    Make a christmas tree using loops

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 let box1 = document.getElementById("zad1")
 for (let i = 0; i < 10; i++) {
     if (i < 5) {
-        box1.innerHTML += "_ " 
+        box1.innerHTML += "_ "
     } else {
         box1.innerHTML += "* "
     }
@@ -12,14 +12,13 @@ for (let i = 0; i < 10; i++) {
 let string = "";
 for (let j = 0; j < 4; j++) {
     for (let i = 0; i < 10; i++) {
-        if (i < 5) 
-        {
-        string += "_ "
+        if (i < 5) {
+            string += "_ "
         } else {
-        string += "* " 
-        }      
-}
-string = string + "\n"
+            string += "* "
+        }
+    }
+    string = string + "\n"
 }
 document.getElementById("zad2").innerHTML = string;
 
@@ -27,13 +26,30 @@ document.getElementById("zad2").innerHTML = string;
 let string1 = "";
 for (let j = 0; j < 10; j++) {
     for (let i = 0; i < 10; i++) {
-    if (i < j + 1) {
-        string1 += "*" 
-    } else {
-        string1 += "-"
-    } 
-}
-string1 = string1 + "\n"
+        if (i <= j) {
+            string1 += "*"
+        } else {
+            string1 += "-"
+        }
+    }
+    string1 = string1 + "\n"
 }
 document.getElementById("zad3").innerHTML = string1;
+
+// ZADANIE 4
+let string2 = "";
+for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < 20; i++) {
+        8 - j < i
+        i > 8 - j
+        if (i > 8 - j && i < 11 + j) {
+            string2 += "+"
+        }
+        else {
+            string2 += "_"
+        }
+    }
+    string2 = string2 + "\n"
+}
+document.getElementById("zad4").innerHTML = string2;
 
